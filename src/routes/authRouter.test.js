9b837bc04107e0be0a9ed640dcbc8e9e693fa4f4
Matthew Nelson <matthewbuzz@gmail.com>
2025-09-23 15:17:07 -0600

    Test authRouter

---
+++ src/routes/authRouter.test.js
@@ -0,0 +1,42 @@
+const request = require('supertest');
+const app = require('../service');
+
+const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+let testUserAuthToken;
+
+beforeAll(async () => {
+  testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+  const registerRes = await request(app).post('/api/auth').send(testUser);
+  testUserAuthToken = registerRes.body.token;
+  expectValidJwt(testUserAuthToken);
+});
+
+test('login', async () => {
+  const loginRes = await request(app).put('/api/auth').send(testUser);
+  expect(loginRes.status).toBe(200);
+  expectValidJwt(loginRes.body.token);
+
+  const expectedUser = { ...testUser, roles: [{ role: 'diner' }] };
+  delete expectedUser.password;
+  expect(loginRes.body.user).toMatchObject(expectedUser);
+});
+
+test('logout', async () => {
+  const logoutRes = await request(app)
+    .delete('/api/auth')
+    .set('Authorization', `Bearer ${testUserAuthToken}`);
+  expect(logoutRes.status).toBe(200);
+  expect(logoutRes.body.message).toBe('logout successful');
+});
+
+test('logout without auth', async () => {
+  const logoutRes = await request(app).delete('/api/auth');
+  expect(logoutRes.status).toBe(401);
+  expect(logoutRes.body.message).toBe('unauthorized');
+});
+
+
+
+function expectValidJwt(potentialJwt) {
+  expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+}
